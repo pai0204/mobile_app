39ba674af8acbca697943dda4eb0e51aac98bb3f
pai0204 <narin2005ft@gmail.com>
2025-02-05 10:50:41 +0700

    Update Page1.dart

--- lib/Page1.dart
+++ lib/Page1.dart
@@ -1,7 +1,7 @@
 // ignore: file_names
 // ignore_for_file: file_names, duplicate_ignore
 
-importyyy 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
 class Page1 extends StatelessWidget {
   final String title;
